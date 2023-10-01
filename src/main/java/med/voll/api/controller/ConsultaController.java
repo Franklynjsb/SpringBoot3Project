300734dad47f92d018b95b68a4e113b3cced0cb9
Frank♥ <98553892+Franklynjsb@users.noreply.github.com>
2023-09-30 22:08:03 -0300

    Commit:Add-Validations-FixErrors

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,12 +1,15 @@
 package med.voll.api.controller;
 
 
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultaService;
 import med.voll.api.domain.consulta.DatosAgendarConsulta;
 import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import med.voll.api.infra.errores.ValidacionDeIntegridad;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +20,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsultaService service;
 
     @PostMapping
     @Transactional
-    public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsulta datos) {
-        System.out.println(datos);
+    public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsulta datos) throws ValidacionDeIntegridad {
 
-        return ResponseEntity.ok(new DatosDetalleConsulta(null,null,null,null));
+        var response = service.agendar(datos);
+
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -4,7 +4,14 @@ package med.voll.api.domain.consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
+
+
+    Boolean existsByPacienteIdAndFechaBetween(Long aLong, LocalDateTime primerHorario, LocalDateTime ultimoHorario);
+
+    Boolean existsByMedicoIdAndFecha(Long aLong, LocalDateTime fecha);
 }
